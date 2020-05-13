b22e123e76b890fda9b9e5cb3bec8d87413656a8
FazliddinKarimov1701 <fazliddinkarimov1701@gmail.com>
2020-05-13 21:15:55 +0500

    first python lesson

---
+++ main.py
@@ -0,0 +1,29 @@
+import turtle
+# fk = turtle.Turtle()
+nk = turtle.Turtle()
+# fk.color("dodgerblue")
+# sides = range(360)
+# for side in sides:
+#     fk.forward(10)
+#     fk.right(360/100)
+# def number(long, degree, sides):
+#     for side in sides:
+#         nk.forward(long)
+#         nk.right(degree)
+# number(100,360/5,range(5))
+# number(70,90,range(4))
+# number(40,120,range(3))
+def large_loop(begin,color):
+    for side in range(5):
+        # nk.right(begin)
+        for side in range(4):
+            nk.color(color)
+            nk.forward(100)
+            nk.right(90)
+        nk.right(20)
+        nk.hideturtle()
+large_loop(0,"cyan")
+large_loop(100,"red")
+large_loop(200,"blue")
+
+
