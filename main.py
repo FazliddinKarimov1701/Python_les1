0a94d5992af70b829a69126c0c4a4aa6e7c8c768
FazliddinKarimov1701 <fazliddinkarimov1701@gmail.com>
2020-05-14 00:12:31 +0500

    variable

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ nk = turtle.Turtle()
 # number(100,360/5,range(5))
 # number(70,90,range(4))
 # number(40,120,range(3))
-def large_loop(begin,color):
-    for side in range(5):
-        # nk.right(begin)
-        for side in range(4):
-            nk.color(color)
-            nk.forward(100)
-            nk.right(90)
-        nk.right(20)
-        nk.hideturtle()
-large_loop(0,"cyan")
-large_loop(100,"red")
-large_loop(200,"blue")
+# def large_loop(begin,color):
+#     for side in range(5):
+#         # nk.right(begin)
+#         for side in range(4):
+#             nk.color(color)
+#             nk.forward(100)
+#             nk.right(90)
+#         nk.right(20)
+#         nk.hideturtle()
+# large_loop(0,"cyan")
+# large_loop(100,"red")
+# large_loop(200,"blue")
 
+print("*" * 20)
+price = 10
+print(price)
 
